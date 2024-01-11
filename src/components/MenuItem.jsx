da1ce46dec869163645d8fc566f39744e5872faa
Noor Mohammed <noormohammedknr2@gmail.com>
2024-01-11 22:41:18 +0530

    added the dark mode to website

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address} className="hover:text-amber-500">
-      <Icon lassNAme="text-2xl sm:hidden" />
+      <Icon className="text-2xl sm:hidden" />
       <p className="uppercase hidden sm:inline text-sm">{title}</p>
     </Link>
   );
