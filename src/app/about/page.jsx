4dae0a791d6ed73b622228b12b463aaa434f9429
Noor Mohammed <noormohammedknr2@gmail.com>
2024-01-11 22:56:08 +0530

    about page done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        ullam eaque modi repellat debitis laboriosam veniam numquam facilis
+        consequatur accusantium! Esse, repellendus voluptatum. Mollitia non nisi
+        nostrum optio, eveniet quo quae saepe fugiat nemo!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, nam
+        optio in odio commodi iste, quas blanditiis inventore accusamus
+        voluptate totam sunt est maiores aliquam laudantium? Voluptatum, magni
+        quas. Ratione ipsa quos, laborum labore temporibus omnis perferendis
+        est, ea beatae distinctio error at recusandae praesentium iusto rerum a
+        odit! Recusandae!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, dolor
+        necessitatibus facere perferendis temporibus asperiores fugiat et error
+        dolore? Quia sint illo eveniet. Officiis eos soluta rem neque dolorem
+        amet perspiciatis eum pariatur. Consectetur, quaerat quam! In expedita
+        harum modi.
+      </p>
+    </div>
+  );
+}
